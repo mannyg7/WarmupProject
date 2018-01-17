20b6e578185836a1cd2a8cb5a30e7bbe35d435fc
Shuxin Zhang <shuxinzhang@shuxins-mbp.attlocal.net>
2018-01-17 13:21:48 -0600

    optimized performance

--- database/hello.go
+++ database/hello.go
@@ -149,12 +149,15 @@ func sign(w http.ResponseWriter, r *http.Request) {
 // [END func_sign]
 
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
 	c := appengine.NewContext(r)
+	log.Infof(c, "request")
 
 	// Read body
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
+		log.Infof(c, "reading body")
 		http.Error(w, err.Error(), 500)
 		return
 	}
@@ -162,7 +165,9 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	var f interface{}
 	// store the json object mapping into f
 	err = json.Unmarshal(b, &f)
+	//log.Infof(c, "body:"+string(b))
 	if err != nil {
+		log.Infof(c, "marshalling: "+err.Error())
 		http.Error(w, err.Error(), 500)
 		return
 	}
@@ -180,38 +185,58 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	count := 0
+	var datastoreKeys []*datastore.Key
+	var datastoreProps []datastore.PropertyList
 
 	// loop through each entry in payload
 	for _, val := range vals {
 		var props datastore.PropertyList
 		vSet := asArray(val)
+		count = count + 1
 		// loop through each key-value pair in each entry
 		for i, v := range vSet {
 			k := asString(keys[i])
-			log.Infof(c, k)
+			//log.Infof(c, k)
 			switch v.(type) {
 			case string:
-				log.Infof(c, asString(v))
+				//log.Infof(c, asString(v))
 				props = append(props, datastore.Property{Name: k, Value: asString(v)})
 			case float64:
-				log.Infof(c, strconv.FormatFloat(asFloat(v), 'f', 5, 64))
+				//log.Infof(c, strconv.FormatFloat(asFloat(v), 'f', 5, 64))
 				props = append(props, datastore.Property{Name: k, Value: asFloat(v)})
 			case int:
-				log.Infof(c, strconv.FormatInt(v.(int64), 10))
+				//log.Infof(c, strconv.FormatInt(v.(int64), 10))
 				props = append(props, datastore.Property{Name: k, Value: asInt(v)})
 			case bool:
-				log.Infof(c, strconv.FormatBool(v.(bool)))
+				//log.Infof(c, strconv.FormatBool(v.(bool)))
 				props = append(props, datastore.Property{Name: k, Value: v.(bool)})
 			default:
 				fmt.Println(k, "is of a type I don't know how to handle")
 			}
+			key := datastore.NewIncompleteKey(c, entityName, nil)
+			_, err = datastore.Put(c, key, props)
 		}
+
 		key := datastore.NewIncompleteKey(c, entityName, nil)
-		datastore.Put(c, key, &props)
+		datastoreKeys = append(datastoreKeys, key)
+		datastoreProps = append(datastoreProps, props)
+
+		if count%1000 == 0 {
+			log.Infof(c, strconv.Itoa(count))
+			log.Infof(c, strconv.Itoa(len(datastoreKeys)))
+			//_, storeerror := datastore.PutMulti(c, datastoreKeys[count-1000:count], datastoreProps[count-1000:count])
+			//if storeerror != nil {
+			//		log.Infof(c, storeerror.Error())
+			//		http.Error(w, storeerror.Error(), 500)
+			//	}
+		}
+
 	}
 
 	output, err := json.Marshal(f)
 	if err != nil {
+		log.Infof(c, "marshalling json")
 		http.Error(w, err.Error(), 500)
 		return
 	}
