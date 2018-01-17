a37cb1e2c533b2f7a8740529487d2b87998b639f
Shuxin Zhang <shuxinzhang@shuxins-mbp.attlocal.net>
2018-01-17 13:46:27 -0600

    Revert "Revert "mocked backend""
    This reverts commit bd11f7a5cf4f99b7e979a001a677d67d34eab29d.

---
+++ mock-backend/mock.py
@@ -0,0 +1,56 @@
+import csv
+import sys
+import requests
+import json
+f = open('sample.csv','rb')
+reader = csv.reader(f)
+i = 0;
+headers = []
+cur = reader.next()
+while len(cur) <= 1:
+	cur = reader.next()
+headers = cur
+vals = []
+i = 0
+for row in reader:
+	i+=1
+	vals.append(row)
+	if (i<10):
+		print row
+
+print len(vals)
+json_req1 = {
+	"body":{
+        "entityName": "TestCount",
+        "keys": headers,
+        "values": vals[1:2000]
+        }
+	}
+json_req2 = {
+	"body":{
+        "entityName": "testJsonBulkeSeq",
+        "keys": headers,
+        "values": vals[20001:40000]
+        }
+	}
+
+json_req3 = {
+	"body":{
+        "entityName": "testJsonBulkeSeq",
+        "keys": headers,
+        "values": vals[40001:60000]
+        }
+	}
+json_req4 = {
+	"body":{
+        "entityName": "testJsonBulkeSeq",
+        "keys": headers,
+        "values": vals[60001:80000]
+        }
+	}
+
+#print json.dumps(json_req)
+r = requests.post('http://localhost:8080/json',json = json_req1)		
+	
+
+f.close()
